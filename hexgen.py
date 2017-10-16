987260740d32f3c5407f21798dfe78321e74dd4a
Axirius <avoncarstien@gmail.com>
2017-10-16 06:52:25 -0500

    minor tweaks investigating geoform issus

--- hexgen.py
+++ hexgen.py
@@ -5,14 +5,14 @@ options = {
     "map_type": MapType.terran,
     "surface_pressure": 1000.25,
     "axial_tilt": 27,
-    "size": 200,
+    "size": 100,
     "base_temp": 0,
     "avg_temp": 25,
-    "sea_percent": 20,
+    "sea_percent": 40,
     "hydrosphere": True,
-    "num_rivers": 350
+    "num_rivers": 125
     #"random_seed": 1234578
 }
 
 gen = generate(options, image=True)
-#gen.export('bin/exportfile.json')
+gen.export('bin/exportfile.json')
