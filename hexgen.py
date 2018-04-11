04b68a394fadec7fbcf944d09b667cdbaa53528b
Axirius <avoncarstien@gmail.com>
2018-04-11 16:19:41 -0500

    stuff

--- hexgen.py
+++ hexgen.py
@@ -5,14 +5,14 @@ options = {
     "map_type": MapType.terran,
     "surface_pressure": 1,
     "axial_tilt": 27,
-    "size": 200,
+    "size": 350,
     "base_temp": 0,
     "avg_temp": 25,
     "sea_percent": 65,
     "hydrosphere": True,
-    "num_rivers": 350,
-    "random_seed": 4649489417720096785
+    "num_rivers": 800
+    #"random_seed": 4649489417720096785
 }
 
 gen = generate(options, image=True)
-#gen.export('bin/exportfile.json')
+gen.export('bin/exportfile.json')
